5d6f91bdbfed79b6a7006d15fe1a82301cbd6d1d
Wendy Su <w696905@gmail.com>
2020-09-16 22:19:28 +0800

    refactor

--- products.py
+++ products.py
@@ -1,33 +1,50 @@
-# 讀取檔案
+
 import os #operating system
 products = []
-if os.path.isfile('products.csv'): #check if the file exists
-    print('Yeah! Find the file!')
-    with open('products.csv', 'r', encoding= 'utf-8') as f:
+# 讀取檔案
+def read_file(filename):
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
             if 'product name, price' in line:
                 continue
             name, price = line.strip().split(',')
             products.append([name, price])
     print(products)
-else:
-    print('Cannot find the file!')
+    return products
 
 #請使用輸入
-while True:
-    name = input('Please enter product name: ')
-    if name == 'q':
-        break
-    price = input('Please enter price: ')
-    products.append([name,price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Please enter product name: ')
+        if name == 'q':
+            break
+        price = input('Please enter price: ')
+        products.append([name,price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print('the price of', p[0], 'is', p[1])
+def print_products(products):
+    for p in products:
+        print('the price of', p[0], 'is', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('product name, price\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename,products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('product name, price\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #check if the file exists
+        print('Yeah! Find the file!')
+        products = read_file(filename)
+    else:
+        print('Cannot find the file!')
+    products = user_input(products)
+    print_products(products)
+    write_file(filename, products)
+
+main()
